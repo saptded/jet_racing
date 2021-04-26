f18b2bf0a10ae0b44924392f2abc01a89f593a2f
dark0ghost <dark0ghostworkmail@seznam.cz>
2021-04-26 22:20:45 +0300

    add rust

--- src/tests/target/debug/deps/tests-0f66c3930c044193.d
+++
@@ -1,5 +0,0 @@
-/home/project/jet_racing/src/tests/target/debug/deps/tests-0f66c3930c044193: src/main.rs
-
-/home/project/jet_racing/src/tests/target/debug/deps/tests-0f66c3930c044193.d: src/main.rs
-
-src/main.rs:
