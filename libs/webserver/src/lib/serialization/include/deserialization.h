58c7a3ce6bcdaa1783d048a30cd851950ef43eec
dark0ghost <dark0ghostworkmail@seznam.cz>
2021-05-25 00:47:45 +0300

    added CustomDeserialization with rapidjson

--- libs/webserver/src/lib/serialization/include/deserialization.h
+++ libs/webserver/src/lib/serialization/include/deserialization.h
@@ -7,28 +7,15 @@
 #include <Position.hpp>
 #include <string>
 
-/*
- * T must have method jsonToVector
- * where return vector: std::string>
- * where index:
- * 0 - username
- * 1 - x
- * 2 - y
- * 3 - z
- */
+
 template<typename T>
 class DeserializationObject{
 
     T jsonTransformer = T();
 
 public:
-    Position getPositionFromJson(const std::string& json){
-        std::vector<std::string> valueFrmJson = jsonTransformer.jsonToVector(json);
-        std::string username = valueFrmJson[0];
-        std::string x = valueFrmJson[1];
-        std::string y = valueFrmJson[2];
-        std::string z = valueFrmJson[3];
-        return Position{username,x,y,z};
+    std::vector<Position> getPositionFromJson(const std::string& json){
+        return jsonTransformer.jsonToPosition(json);
     }
 };
 
