a27eb62c68fe1b082d3b17360071f4bdb3d92a88
KamilaNurkhametova <nkf99@mail.ru>
2021-05-26 15:20:39 +0300

    written test for arc 80%

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -13,7 +13,7 @@ std::vector<playerPosition> getPlayerCoordinates() {
     std::vector<playerPosition> playerCoordinates;
 
     playerPosition playerOrthogonal = {Point(2, 2), Point(5, 2), Point(5, 6), Point(2, 6)};
-    playerPosition playerAtAngle = {Point(4, 2), Point(8, 5), Point(6,7), Point(2, 4)};
+    playerPosition playerAtAngle = {Point(4, 2), Point(8, 5), Point(6, 7), Point(2, 4)};
 
     playerCoordinates.push_back(playerOrthogonal);
     playerCoordinates.push_back(playerAtAngle);
@@ -34,8 +34,10 @@ TEST(TestIntersection, PlayerToLineNoIntersect) {
     std::vector<playerPosition> playerCoordinates = getPlayerCoordinates();
 
     for (auto &line : linesTest) {
-        EXPECT_FALSE(line.intersect(playerCoordinates[0].playerTL, playerCoordinates[0].playerTR, playerCoordinates[0].playerBL, playerCoordinates[0].playerBR));
-        EXPECT_FALSE(line.intersect(playerCoordinates[1].playerTL, playerCoordinates[1].playerTR, playerCoordinates[1].playerBL, playerCoordinates[1].playerBR));
+        EXPECT_FALSE(
+            line.intersect(playerCoordinates[0].playerTL, playerCoordinates[0].playerTR, playerCoordinates[0].playerBL, playerCoordinates[0].playerBR));
+        EXPECT_FALSE(
+            line.intersect(playerCoordinates[1].playerTL, playerCoordinates[1].playerTR, playerCoordinates[1].playerBL, playerCoordinates[1].playerBR));
     }
 }
 
@@ -55,3 +57,63 @@ TEST(TestIntersection, PlayerToLineIntersect) {
     }
 }
 
+TEST(TestIntersection, PlayerToArcNoIntersect) {
+    Arc upArcToTheLeft({0, 4}, {6, 4}, {3, 4});
+    Arc upArcToTheRight({1, 4}, {7, 4}, {4, 4});
+    Arc bottomArcToTheLeft({6, 4}, {0, 4}, {3, 4});
+    Arc bottomArcToTheRight({7, 4}, {1, 4}, {4, 4});
+    Arc leftArcToTheLeft({3, 7}, {3, 1}, {3, 4});
+    Arc leftArcToTheRight({4, 7}, {4, 1}, {4, 4});
+    Arc rightArcToTheLeft({3, 1}, {3, 7}, {3, 4});
+    Arc rightArcToTheRight({4, 1}, {4, 7}, {4, 4});
+
+    std::vector<Arc> linesTestOrthPlayer{upArcToTheLeft,   upArcToTheRight,   bottomArcToTheLeft, bottomArcToTheRight,
+                                         leftArcToTheLeft, leftArcToTheRight, rightArcToTheLeft,  rightArcToTheRight};
+
+    Arc upArcAbove({1, 4}, {9, 4}, {5, 4});
+    Arc upArcBelow({1, 5}, {9, 5}, {5, 5});
+    Arc bottomArcAbove({9, 4}, {1, 4}, {5, 4});
+    Arc bottomArcTBelow({9, 5}, {1, 5}, {5, 5});
+    Arc leftArcAbove({5, 8}, {5, 0}, {5, 4});
+    Arc leftArcBelow({5, 9}, {5, 1}, {5, 5});
+    Arc rightArcAbove({5, 0}, {5, 8}, {5, 4});
+    Arc rightArcBelow({5, 1}, {5, 9}, {5, 5});
+
+    std::vector<Arc> linesTestAtAnglePlayer{upArcAbove, upArcBelow, bottomArcAbove, bottomArcTBelow, leftArcAbove, leftArcBelow, rightArcAbove, rightArcBelow};
+
+    std::vector<playerPosition> playerCoordinates = getPlayerCoordinates();
+
+    for (auto &line : linesTestOrthPlayer) {
+        EXPECT_FALSE(
+            line.intersect(playerCoordinates[0].playerTL, playerCoordinates[0].playerTR, playerCoordinates[0].playerBL, playerCoordinates[0].playerBR));
+    }
+
+    for (auto &line : linesTestAtAnglePlayer) {
+        EXPECT_FALSE(
+            line.intersect(playerCoordinates[1].playerTL, playerCoordinates[1].playerTR, playerCoordinates[1].playerBL, playerCoordinates[1].playerBR));
+    }
+}
+
+TEST(TestIntersection, PlayerToArcIntersect) {
+    Arc upArc({2, 2}, {5, 2}, {3.5, 2});
+    Arc bottomArc({5, 6}, {2, 6}, {3.5, 6});
+    Arc rightArc({5, 2}, {5, 6}, {5, 4});
+    Arc leftArc({2, 6}, {2, 2}, {2, 4});
+    Arc topLeftArc({2, 6}, {5, 2}, {3.5, 4});
+    Arc topRightArc({2, 2}, {5, 6}, {3.5, 4});
+    Arc bottomLeftArc({5, 6}, {2, 2}, {3.5, 4});
+    Arc bottomRightArc({5, 2}, {2, 6}, {3.5, 4});
+
+    std::vector<Arc> linesTestOrthPlayer{upArc, bottomArc, rightArc, leftArc, topLeftArc, topRightArc, bottomLeftArc, bottomRightArc};
+
+    std::vector<playerPosition> playerCoordinates = getPlayerCoordinates();
+
+    for (auto &line : linesTestOrthPlayer) {
+        EXPECT_TRUE(line.intersect(playerCoordinates[0].playerTL, playerCoordinates[0].playerTR, playerCoordinates[0].playerBL, playerCoordinates[0].playerBR));
+    }
+
+    //    for (auto &line : linesTestAtAnglePlayer) {
+    //        EXPECT_FALSE(
+    //            line.intersect(playerCoordinates[1].playerTL, playerCoordinates[1].playerTR, playerCoordinates[1].playerBL, playerCoordinates[1].playerBR));
+    //    }
+}
